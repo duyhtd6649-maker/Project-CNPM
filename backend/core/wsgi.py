56dc1d7baea0831f459a84ad46a303a533ee366c
Huynh Duy <duyhtd6649@ut.edu.vn>
2025-12-13 20:44:45 +0700

    Update khung, chua fix docker

--- backend/core/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Trỏ về 'core.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-application = get_wsgi_application()
--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello! CareerMate is running!</h1>
-    </div>
-  );
-}
-export default App;
