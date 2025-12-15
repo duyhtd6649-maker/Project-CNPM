88deedcd0e1be0292060811e2e959cc8ee6ebfb3
Huynh Duy <duyhtd6649@ut.edu.vn>
2025-12-15 18:27:58 +0700

    update khung

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'careermate_db',      
+        'USER': 'root',
+        'PASSWORD': 'HuynhDuy_123',  
+        'HOST': os.getenv('DB_HOST', 'localhost'),     
+        'PORT': '3306',
     }
 }
 
