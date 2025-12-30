b47c14b14f3dc7612b718076475416d5533daaca
DungChau523 <chauquocdung072906@gmail.com>
2025-12-30 13:56:11 +0700

    update jwt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('user/',views.GetUserInfor),
     path('user/add/',views.AddUser),
     path('user/register/',views.UserRegister),
     path('user/<str:username>/',views.GetUserbyUsername),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
