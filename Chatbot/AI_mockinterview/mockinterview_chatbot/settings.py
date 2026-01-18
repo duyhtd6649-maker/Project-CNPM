9d629b6207e0dc11ca140366135555ffba858eb0
Pham Ba Tri Tam <tampbt3039@ut.edu.vn>
2026-01-18 11:11:37 +0700

    Hoan thien chatbot

--- Chatbot/AI_mockinterview/mockinterview_chatbot/settings.py
+++ Chatbot/AI_mockinterview/mockinterview_chatbot/settings.py
@@ -1,13 +1,9 @@
 from pathlib import Path
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 load_dotenv(BASE_DIR / ".env")
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
