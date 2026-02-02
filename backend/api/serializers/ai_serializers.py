d944494558c3fa2b771c35c98c46ba2dbc5f03b7
Huynh Duy <duyhtd6649@ut.edu.vn>
2026-02-03 04:58:00 +0700

    sua lai ai

--- backend/api/serializers/ai_serializers.py
+++ backend/api/serializers/ai_serializers.py
@@ -8,7 +8,7 @@ class CareerCoachResponseSerializer(serializers.Serializer):
     expectedCareer = serializers.ListField(child=serializers.CharField())
     overview = serializers.ListField(child=serializers.CharField())
     skills = serializers.ListField(child=serializers.CharField())
-    learningPaths = serializers.ListField(child=serializers.CharField())
+    learningPaths = serializers.ListField(child=serializers.DictField())
 
 
 class CvAnalyzerRequestSerializer(serializers.Serializer):
