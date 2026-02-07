c047d38e06f428c46fc8abf015c925a4af4804d6
Huynh Duy <duyhtd6649@ut.edu.vn>
2026-02-07 20:29:05 +0700

    chinh lai response application

--- backend/api/serializers/application_serializers.py
+++ backend/api/serializers/application_serializers.py
@@ -3,9 +3,13 @@ from database.models.jobs import Applications
 
 class ApplicationSerializer(serializers.ModelSerializer):
     job_title = serializers.CharField(source = 'job.title', read_only = True)
+    company = serializers.CharField(source = 'job.company.name',read_only = True)
+    user_name = serializers.CharField(source = 'candidate.user.fullname', read_only = True)
+    user_phone = serializers.CharField(source = 'candidate.user.phone', read_only = True)
+    user_email = serializers.CharField(source = 'candidate.user.email', read_only = True)
     class Meta:
         model = Applications
-        fields = ['id', 'job_id', 'cvsid', 'job_status','system_status', 'job_title']
+        fields = ['id', 'job_id', 'cvsid', 'job_status','system_status', 'job_title','created_date','company','user_name','user_phone','user_email']
         read_only_fields = ['id','job_status','system_status']
 
 class ApplicationSystemStatusUpdateSerializer(serializers.Serializer):
